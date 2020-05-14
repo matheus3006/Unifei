471e3c76d6a1589b0b7f03dd8c46233faf6672d0
João Lucas Ribeiro <54084537+joaolucas2012@users.noreply.github.com>
2020-05-13 21:43:58 -0300

    New Python codes added.

--- Git Codes/Git codes/Python/Exercises/ex8_temp.py
+++
@@ -1,8 +0,0 @@
-def celsius_to_farenheit(celsius):
-    return celsius * (9/5) + 32
-
-cel = input('Digite a temperatura em celsius: ')
-far = celsius_to_farenheit(float (cel))
-
-print(cel + ' C equivale a ' + str(far) + ' F')
-    
